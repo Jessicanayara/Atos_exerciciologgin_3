ce88a224e669d42bde8e2a4ec4637bc6dd20e979
Jessicanayara <jjessyka.nay@gmail.com>
2023-05-01 18:57:57 -0300

    Adicionado funcionalidade de log

--- BancoDeDados.java
+++ BancoDeDados.java
@@ -9,19 +9,48 @@ import java.sql.ResultSet;
 
 import java.sql.SQLException;
 
-import java.util.Scanner;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 
 public class BancoDeDados implements InterfaceBancoDeDados {
   
     private Connection connection;
-    public static void main(String[] args) {
+    private static Log meuLogger;
+    public static void main(String[] args) throws IOException {
+        
+        
+    try {
+        meuLogger = new Log("Log.txt");
+        
+        meuLogger.logger.setLevel(Level.FINE);
+        meuLogger.logger.info("Log de informação");
+        meuLogger.logger.warning("Log de Aviso");
+        meuLogger.logger.severe("Log Severo");
+        
+
+    } catch (Exception e) {
+        meuLogger.logger.info("Exception:" + e.getMessage()); //escrever no arquivo de log o erro
+        e.printStackTrace();//escrever no console o erro
+
+    }
+
+
+
+    meuLogger.logger.info("\n iniciando Banco de dados");
 
         BancoDeDados bd = new BancoDeDados();
+        meuLogger.logger.info("\n iniciando Banco de dados");
         bd.conectar("jdbc:mysql://localhost:3306/reuniao", "root", "") ;
+        meuLogger.logger.info("\n iniciando consulta com Banco de dados");
         bd.consultar("SELECT * FROM pessoa");
+        meuLogger.logger.info("\n iniciando alteração no Banco de dados");
        
-        bd.inserirAlterarExcluir("INSERT INTO pessoa(nome,email,cargo) values ('Adriana Santos','adri_s@yahoo.com', 'Diretora')");
-
+        bd.inserirAlterarExcluir("INSERT INTO pessoa(nome,email,cargo) values ('Bruna Santos','bru_s@yahoo.com', 'Supervisora')");
+        meuLogger.logger.info("\n Desconectando o Banco de dados");
         bd.desconectar();
 
         
@@ -34,8 +63,10 @@ public class BancoDeDados implements InterfaceBancoDeDados {
 
     @Override
     public void conectar(String db_url, String db_user, String db_password)  {
-
+        
+        
         try{
+          meuLogger.logger.warning("conectando ao banco");
             System.out.println("Iniciando conexão");
            
             connection= DriverManager.getConnection(db_url, db_user, db_password);
@@ -44,7 +75,7 @@ public class BancoDeDados implements InterfaceBancoDeDados {
     
         }catch (SQLException e) {
     
-            
+            meuLogger.logger.severe("erro" + e);
             System.out.println("Nao foi possível conectar ao BD"+e);
             
             }
@@ -54,9 +85,11 @@ public class BancoDeDados implements InterfaceBancoDeDados {
     @Override
     public void desconectar() {
         try {
+            meuLogger.logger.warning("desconectando ao banco");
             connection.close();
             System.out.println("Desconectado");
         } catch (SQLException e) {
+            meuLogger.logger.severe("erro" + e);
             System.out.println("Erro ao fechar conexão com o banco de dados: " + e.getMessage());
         }
     }
@@ -69,6 +102,7 @@ public class BancoDeDados implements InterfaceBancoDeDados {
     @Override
     public void consultar(String db_query) {
         try{
+            meuLogger.logger.warning("consultando banco");
             java.sql.Statement statement = connection.createStatement();
         
     
@@ -84,7 +118,7 @@ public class BancoDeDados implements InterfaceBancoDeDados {
      
      }catch (SQLException e) {
     
-            
+        meuLogger.logger.severe("erro" + e);    
         System.out.println("Nao foi possível conectar ao BD"+e);
         
         }
@@ -100,6 +134,7 @@ public class BancoDeDados implements InterfaceBancoDeDados {
     public int inserirAlterarExcluir(String db_query) {
        int linhas=0;
         try{
+            meuLogger.logger.warning("inserindo ao banco");
             java.sql.Statement statement = connection.createStatement();
         
             linhas = statement.executeUpdate(db_query);
@@ -107,7 +142,7 @@ public class BancoDeDados implements InterfaceBancoDeDados {
      
      }catch (SQLException e) {
     
-            
+        meuLogger.logger.severe("erro" + e);    
         System.out.println("Nao foi possível inserir, alterar ou excluir"+" "+e);
         
         }
